f86c424a183a58cfe4ba0d405c87820252344493
MikeYC-Wang <asd384639@gmail.com>
2026-03-07 14:56:47 +0800

    環境設定

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bento-bg': '#F4F1DE',      
+        'bento-primary': '#E9C46A', 
+        'bento-secondary': '#2A9D8F',
+        'bento-accent': '#E76F51',  
+      },
+    },
   },
   plugins: [],
 }
-
